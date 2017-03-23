a14c3993de5a03253e0c5aa3acaad5046d5e3ea6
mariana-martins <mariana_martins@id.uff.br>
2017-03-23 16:28:42 +1300

    Adding new informations and creating a new model fuction

--- js/app.js
+++ js/app.js
@@ -1,10 +1,35 @@
-var ViewModel = function () {
+var Cat = function () {
     this.clickCount = ko.observable(0);
     this.name = ko.observable("Jessie");
     this.imgSrc = ko.observable("img/cat-1.jpg");
+    this.nicknames = ko.observableArray(["J", "Jess", "Mr. J"]);
+
+    this.title = ko.computed(function() {
+        var title;
+        var clicks = this.clickCount();
+        if (clicks < 10) {
+            title = "Newborn";
+        } else if (clicks < 50) {
+            title = "Infant";
+        } else if (clicks < 100) {
+            title = "Child";
+        } else if (clicks < 200) {
+            title = "Teenage";
+        } else if (clicks < 300) {
+            title = "Infant";
+        } else {
+            title = "Ninja";
+        }
+        return title;
+    }, this);
+
+};
+
+var ViewModel = function () {
+    this.currentCat = ko.observable(new Cat());
 
     this.incrementCounter = function() {
-        this.clickCount( this.clickCount() + 1);
+        this.currentCat().clickCount( this.currentCat().clickCount() + 1);
     }
 };
 
